c0453620dd23059f25c8d2fbd16fa77f87c68a89
ondarigidion <gidion.ondari@student.moringaschool.com>
2025-06-12 13:13:30 +0300

    dne

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+//Define the current user
+const currentUser = 'Grace Hopper';
+
+// Welcome message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//  Excited welcome message
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+//  Short greeting using the first letter of currentUser
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
